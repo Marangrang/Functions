641b990b114672fb1cf4171dc149f077acb7b054
Marangrang Tsepetsi <Marangrang@explore-datascience.net>
2019-03-19 15:17:08 +0200

    First commit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Rangz',
+    name='Functions',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/Marangrang/Rangz.git',
+    url='https://github.com/Marangrang/Functions.git',
     author='marangrang Tsepetsi',
     author_email='mtsepetsi2@gmail.com'
 )
