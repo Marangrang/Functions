961e0859989ccf07863777736ac33f69ec7da3f9
Marangrang Tsepetsi <Marangrang@explore-datascience.net>
2019-03-19 15:07:22 +0200

    upgraded version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='Rangz',
-    version='0.1.1',
+    version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/Marangrang/Rangz',
+    url='https://github.com/Marangrang/Rangz.git',
     author='marangrang Tsepetsi',
     author_email='mtsepetsi2@gmail.com'
 )
