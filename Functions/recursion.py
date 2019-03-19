287c3126bc2cd9124ec8f405e89f8863989fb75e
Marangrang Tsepetsi <Marangrang@explore-datascience.net>
2019-03-19 15:34:02 +0200

    first commit

--- Functions/recursion.py
+++ Functions/recursion.py
@@ -19,7 +19,7 @@ def factorial(n):
     """Function to return the factorial
    of a number using recursion"""
     if n == 0:
-        return
+        return 1
     else:
         '''Return n!'''
         return n * factorial(n-1)
